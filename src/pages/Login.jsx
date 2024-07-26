7a917febc93f038c858889003b43688bd115ac4a
SeongYun_Cheon <1112csy@naver.com>
2024-07-26 18:52:31 +0900

    로그인페이지 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/login.css';
+import characterLogo from '../imgs/character_logo_yellow_back.png';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -10,7 +12,7 @@ const Login = () => {
     event.preventDefault();
     console.log('Email:', email);
     console.log('Password:', password);
-    // 여기서 백엔드 API와 연동하여 로그인 처리 로직을 추가 가능쓰
+    // Here you can add logic to connect with backend API for login
   };
 
   const handleSignUpClick = () => {
@@ -18,32 +20,51 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className="login-container">
+      <div className="login-form-container">
+        <div className="login-logo">
+          <img src={characterLogo} alt="Photato Logo" />
+          <h1>Welcome to PHOTATO!</h1>
+          <p>Sign in to continue access pages</p>
         </div>
+        <div className="right-container">
         <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">아이디</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="이메일을 입력하세요."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">비밀번호</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="비밀번호를 입력하세요."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <p className="forgot-password">비밀번호를 잊으셨나요?</p>
+            <button type="submit" className="login-button">로그인하기</button>
+          </form>
+          <button onClick={handleSignUpClick} className="signup-button">회원가입하기</button>
+          <p className="social-login-text">소셜아이디로 간편하게 로그인할 수 있습니다.</p>
+          <div className="social-login-buttons">
+            <button className="social-button naver">네이버로 로그인</button>
+            <button className="social-button kakao">카카오로 로그인</button>
+            <button className="social-button google">구글로 로그인</button>
+          </div>
+          </div>
         </div>
-        <button type="submit">Login</button>
-      </form>
-      <button onClick={handleSignUpClick}>Sign Up</button>
+      </div>
     </div>
   );
 };
