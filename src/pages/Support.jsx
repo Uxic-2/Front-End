7c03f36474ce1b76e774211879d82cfa742d882d
SeongYun_Cheon <1112csy@naver.com>
2024-07-30 13:20:25 +0900

    지원페이지 사이드바 틀 및 홈화면 뷰포트

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Support = () => {
   return (
     <div>
+      <aside className="sidebar">
+          <div className="side1">문의사항</div>
+          <div className="side2">공지사항</div>
+          <div className="side3">자주 묻는 질문</div>
+        </aside>
       <h1>support Page</h1>
     </div>
   );
