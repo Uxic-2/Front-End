d8cc60c6ac1b4820cf308ef532134151cc4470f2
SeongYun_Cheon <1112csy@naver.com>
2024-08-03 17:12:37 +0900

    support 2,3페이지 레이아웃 구현

--- src/pages/SupNotice.jsx
+++ src/pages/SupNotice.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/sidebar.css';
+import '../styles/support/supportNotice.css';
 
 const SupNotice = () => {
   return (
-    <div>
+    <div className="container">
       <aside className="sidebar">
-      <Link to="/support" className="sidebar-item side1 ">문의사항</Link>
-        <Link to="/SupNotice" className="sidebar-item side2 active">공지사항</Link>
-        <Link to="/SupQuestion" className="sidebar-item side3">자주 묻는 질문</Link>
+        <Link to="/support" className="sidebar-item side1">문의사항</Link>
+        <Link to="/supnotice" className="sidebar-item side2 active">공지사항</Link>
+        <Link to="/supquestion" className="sidebar-item side3 ">자주 묻는 질문</Link>
       </aside>
-      <h1>ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ공지사항</h1>
-      {/* Add your notice content here */}
+      <div className="content">
+        <h1>공지사항</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="검색" />
+          <button type="submit">         🔍</button>
+        </div>
+        <table className="faq-table">
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>구분</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
+              <th>조회</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Add your table rows here */}
+          </tbody>
+        </table>
+        <div className="pagination">
+          <button>{"<"}</button>
+          <span className="page-number">1</span>
+          <button>{">"}</button>
+        </div>
+      </div>
     </div>
   );
 };
