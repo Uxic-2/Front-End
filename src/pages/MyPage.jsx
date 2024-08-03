c60fcacf42a126a06ecfdb7af67324b2521b4b3d
junsy0901 <155941121+junsy0901@users.noreply.github.com>
2024-08-04 01:32:50 +0900

    change mypage

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import "../styles/mypage.css";
 
 import map_icon from "../imgs/mypage_map_icon.svg";
+import cursor_icon from "../imgs/mypage_cursor_icon.svg";
 
 function MySidebar() {
   return (
@@ -28,6 +30,31 @@ function MySidebar() {
   );
 }
 
+function ReadyTravelNoSideBar() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className="flex">
+      <div className="flex-grow p-4">
+        <div className="flex justify-center space-x-8">
+          <Calendar
+            onChange={setDate}
+            value={date}
+            selectRange={true}
+            showDoubleView={true}
+            className="p-4 border border-gray-300 rounded"
+          />
+        </div>
+        <div className="flex justify-center mt-4">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            완료
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function MyMap() {
   return (
     <>
@@ -38,15 +65,33 @@ function MyMap() {
             <img className="my-map-icon" src={map_icon}></img>
             <h2 className="my-map-header">내 지도</h2>
           </div>
-          <div className="my-map-section"></div>
+          <div className="my-map-section">이 자리에 지도</div>
         </div>
         <div className="my-map-container">
-          <div className="my-map-upload-text">
+          <div className="my-map-upload-bold-text">
             여행지 사진을 업로드하여
             <br />
             나의 지도를 채워보세요
           </div>
           <button className="my-map-upload-button">upload</button>
+        </div>
+        <div className="my-map-container">
+          <div className="my-map-section">
+            <img
+              className="my-cursor_icon"
+              src={cursor_icon}
+              alt="cursor_icon"
+            />
+            <div className="my-map-upload-bold-text">혹시 여행 중이신가요?</div>
+            <div className="my-map-upload-text">
+              PHOTATO와 함께 여행 계획을 세우지 않았다면
+              <br />
+              여행 경로를 파악할 수 있도록
+              <br />
+              여행 시작 날짜와 끝나는 날짜를 입력해주세요
+            </div>
+          </div>
+          <ReadyTravelNoSideBar />
         </div>
       </div>
     </>
