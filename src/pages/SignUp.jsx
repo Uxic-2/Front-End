7de5b7b32b6898c4dfb55c7b79fe6b6c37572d52
SeongYun_Cheon <1112csy@naver.com>
2024-08-06 18:33:28 +0900

    회원가입페이지 제작

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,54 +1,95 @@
 import React, { useState } from 'react';
+import '../styles/signup.css';
 
 const SignUp = () => {
   const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthdate, setBirthdate] = useState('');
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Name:', name);
+    console.log('Name', name);
+    console.log('Phone:', phone);
+    console.log('Birthdate:', birthdate);
     console.log('Email:', email);
+    console.log('Username:', username);
     console.log('Password:', password);
     // 여기서 백엔드 API와 연동하여 회원가입 처리 로직을 추가할 수 있습니다.
   };
 
   return (
-    <div>
-      <h1>회원가입 페이지</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">이름:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">이메일:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">비밀번호:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+    <div className="background-signup">
+      <div className="signup-container">
+        <h1>Join us with Photato</h1>
+        <form onSubmit={handleSubmit} className="signup-form">
+          <div className="form-group">
+            <label htmlFor="name">이름</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">전화번호</label>
+            <input
+              type="text"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birthdate">생년월일</label>
+            <input
+              type="date"
+              id="birthdate"
+              value={birthdate}
+              onChange={(e) => setBirthdate(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">이메일</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">아이디</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="signup-button">
+            Sign Up
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
