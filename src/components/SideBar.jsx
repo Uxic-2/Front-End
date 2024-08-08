46a0821da8725bbd51d12b714ad81d25e1f22317
junsy0901 <155941121+junsy0901@users.noreply.github.com>
2024-08-08 16:27:10 +0900

    SideBar css 통일(HotSpot, Mypage), Mypage css 수정, color 커스텀 추가

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/sidebar.css";
 
 const SideBar = () => {
   return (
-    <div className="w-64 bg-gray-200 p-4">
+    <div className="w-200 bg-white p-5">
       <ul>
-        <li className="mb-4"><Link to="/hot-spot">STEP 1 사진 선택</Link></li>
-        <li className="mb-4"><Link to="/ready-travel">STEP 2 여행 준비</Link></li>
-        <li className="mb-4"><Link to="/lodging">STEP 3 숙소/지점 추천</Link></li>
-        <li className="mb-4"><Link to="/restaurant">맛집 추천 테스트용</Link></li>
-        <li className="mb-4">STEP 4 이동 경로 안내</li>
-        <li className="mb-4"><Link to="/hidden-spot">Hiddenspot</Link></li>
+        <li className="mb-4 text-main-green font-bold">
+          <NavLink to="/hot-spot">STEP 1 사진 선택</NavLink>
+        </li>
+        <li className="mb-4 text-main-green font-bold">
+          <NavLink to="/ready-travel">STEP 2 여행 준비</NavLink>
+        </li>
+        <li className="mb-4 text-main-green font-bold">
+          <NavLink to="/lodging">STEP 3 숙소/지점 추천</NavLink>
+        </li>
+        <li className="mb-4 text-main-green font-bold">
+          <NavLink to="/restaurant">맛집 추천 테스트용</NavLink>
+        </li>
+        <li className="mb-4 text-main-green font-bold">
+          STEP 4 이동 경로 안내
+        </li>
+        <li className="mb-4 text-main-green font-bold">
+          <NavLink to="/hidden-spot">Hiddenspot</NavLink>
+        </li>
       </ul>
     </div>
   );
-}
+};
 
 export default SideBar;
