f420990b7e1ba58f422bf4162a3be3a0e32b6173
junsy0901 <155941121+junsy0901@users.noreply.github.com>
2024-08-20 01:11:32 +0900

    mypage 컴포넌트들 주소 변경

--- src/components/SideBar/SBMypage.jsx
+++ src/components/SideBar/SBMypage.jsx
@@ -1,9 +1,9 @@
 // Mypage 컴포넌트에서 사용하는 사이드바 내용
 
 const links = [
-  { to: "/mypage", text: "내 지도" },
-  { to: "/mypage/myfolder", text: "내 여행 폴더" },
-  { to: "/mypage/myschedule", text: "저장한 스케줄" },
+  { to: "/mymap", text: "내 지도" },
+  { to: "/myfolder", text: "내 여행 폴더" },
+  { to: "/myschedule", text: "저장한 스케줄" },
 ];
 
 export default links;
