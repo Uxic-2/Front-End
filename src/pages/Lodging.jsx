63b6d62a2b10634877e81ba60ed32e5ee72d022a
SYEONIH <syeonih@naver.com>
2024-08-28 15:05:09 +0900

    수정

--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -4,6 +4,9 @@ import SideBar from "../components/SideBar";
 import links from "../components/SideBar/SBHotspot";
 
 const Lodging = () => {
+  const buttonClass =
+    "px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500";
+
   return (
     <div className="flex">
       <SideBar links={links} />
@@ -11,29 +14,15 @@ const Lodging = () => {
         <h1 className="text-2xl font-bold mb-6">편집자가 추천해주는 숙소 BEST</h1>
         <h1 className="text-xl font-bold mb-6">카테고리</h1>
         <div className="flex gap-4 mb-6">
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            전체
-          </button>
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            호텔, 리조트
-          </button>
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            모텔
-          </button>
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            펜션
-          </button>
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            홈, 빌라
-          </button>
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            캠핑
-          </button>
-          <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white active:bg-orange-500">
-            게하, 한옥
-          </button>
+          <button className={buttonClass}>전체</button>
+          <button className={buttonClass}>호텔, 리조트</button>
+          <button className={buttonClass}>모텔</button>
+          <button className={buttonClass}>펜션</button>
+          <button className={buttonClass}>홈, 빌라</button>
+          <button className={buttonClass}>캠핑</button>
+          <button className={buttonClass}>게하, 한옥</button>
         </div>
-        
+        {/* Dropdown filter */}
         <div className="flex justify-end mb-6">
           <select className="border rounded p-2" defaultValue="거리순">
             <option value="거리순">거리순</option>
@@ -48,12 +37,8 @@ const Lodging = () => {
               <p className="text-gray-700">서울시 노원구 노원로</p>
               <p className="text-gray-700">가격</p>
               <div className="flex items-center mt-2">
-                <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded mr-2 hover:bg-orange-500 hover:text-white">
-                  선택
-                </button>
-                <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white">
-                  ❤️
-                </button>
+                <button className={`${buttonClass} mr-2`}>선택</button>
+                <button className={buttonClass}>❤️</button>
               </div>
             </div>
           </div>
@@ -64,12 +49,8 @@ const Lodging = () => {
               <p className="text-gray-700">서울시 노원구 노원로</p>
               <p className="text-gray-700">가격</p>
               <div className="flex items-center mt-2">
-                <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded mr-2 hover:bg-orange-500 hover:text-white">
-                  선택
-                </button>
-                <button className="px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-orange-500 hover:text-white">
-                  ❤️
-                </button>
+                <button className={`${buttonClass} mr-2`}>선택</button>
+                <button className={buttonClass}>❤️</button>
               </div>
             </div>
           </div>
