9db60d9a472a466da65d9160c4f10df7beaf7251
SeongYun_Cheon <1112csy@naver.com>
2024-08-31 12:38:27 +0900

    물음표아이콘 배치 및 툴팁 수정

--- src/pages/Hotspot.jsx
+++ src/pages/Hotspot.jsx
@@ -4,7 +4,7 @@ import SideBar from "../components/SideBar";
 import links from "../components/SideBar/SBHotspot";
 import Modal from "./HotspotModal";
 import HiddenSpot from "./HiddenSpot";
-import questionIcon from "../imgs/question.png"; 
+import questionIcon from "../imgs/question.png"; // 이미지 경로 import
 
 const HotSpot = () => {
   const hotSpots = Array(9)
@@ -34,15 +34,15 @@ const HotSpot = () => {
         {currentComponent === "HotSpot" ? (
           <>
             <div className="flex justify-between items-center mb-6">
-              <div className="relative">
+              <div className="flex items-center relative">
                 <h2 className="text-2xl">지금 인기있는 HOT SPOT</h2>
-                <div className="absolute -top-2 right-0 flex items-center">
+                <div className="tooltip-icon ml-2">
                   <img
                     src={questionIcon}
                     alt="Question"
-                    className="tooltip-icon ml-2 w-6 h-6"
+                    className="w-6 h-6"
                   />
-                  <div className="tooltip-text absolute bg-gray-800 text-white text-sm p-2 rounded shadow-lg -mt-2 -ml-16 w-72">
+                  <div className="tooltip-text -mt-2 -ml-16 w-72">
                     STEP 1에서는 타 유저들의 여행 사진을 모아두어 한 눈에
                     확인하실 수 있습니다. 하트 버튼을 누를 시 여행 폴더에
                     저장하실 수 있고, 선택 버튼을 누르시면 HIDDEN SPOT으로
