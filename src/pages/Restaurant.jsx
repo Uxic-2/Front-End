d4e51973488f797a7b8ca82ccb0fd0d6074ed3a6
junsy0901 <155941121+junsy0901@users.noreply.github.com>
2024-09-24 20:54:54 +0900

    가운데 정렬, 디자인 통일 등등

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -2,24 +2,53 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SideBar from "../components/SideBar";
 import links from "../components/SideBar/SBHotspot";
+import lodging_photato from "../imgs/lodging_photato.png"; // Lodging photo import
 
 const Restaurant = () => {
+  const buttonClass =
+    "px-4 py-2 bg-white text-black border border-gray-300 rounded hover:bg-main-orange hover:text-white active:bg-main-orange";
+
   return (
     <div className="flex">
       <SideBar links={links} />
-      <div className="ml-64 p-6">
-        <h1 className="text-2xl font-bold mb-6">
-          편집자가 추천해주는 맛집 BEST
-        </h1>
+      <div className="mx-auto mt-5 p-4">
+        <div className="flex flex-col items-left mb-6">
+          <img
+            src={lodging_photato}
+            alt="Lodging Photo"
+            className="w-32 h-auto mb-2"
+          />
+          <h1 className="text-2xl font-bold flex items-center mb-6">
+            <span>
+              <i>편집자가 추천해주는 맛집 BEST</i>
+            </span>
+          </h1>
+        </div>
         <div className="flex gap-4 mb-6">
-          <button className="px-4 py-2 bg-gray-200 rounded">전체</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">중식</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">일식</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">양식</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">한식</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">카페</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">디저트</button>
-          <button className="px-4 py-2 bg-gray-200 rounded">인스타 핫플</button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            전체
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            중식
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            일식
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            양식
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            한식
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            카페
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            디저트
+          </button>
+          <button className={`${buttonClass}px-4 py-2 bg-gray-200 rounded`}>
+            인스타 핫플
+          </button>
         </div>
         <div className="flex flex-col gap-4">
           <div className="flex p-4 bg-white border border-gray-300 rounded">
